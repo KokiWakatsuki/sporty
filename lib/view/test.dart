56f9555e33b643212416826cda0d67424361636e
Koki Wakatsuki <hasukiiq@gmail.com>
2022-11-27 21:59:56 +0900

    動画撮影できた 実機で動かす時はassembledebug出ても気にしない デバッグコンソール観察すれば分かる たまにカメラの初期化失敗するけどアンストしてやり直すとできたりする 自宅PC

--- lib/view/test.dart
+++ lib/view/test.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 // 写真撮影画面
 class Test extends StatefulWidget {
@@ -17,79 +20,48 @@ class Test extends StatefulWidget {
 
 
 class TestState extends State<Test> {
-  late CameraController _controller;
-  // ignore: unused_field
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = CameraController(
-      // カメラを指定
-      widget.camera,
-      // 解像度を定義
-      ResolutionPreset.max,
-    );
-
-    // コントローラーを初期化
-    _initializeControllerFuture = _controller.initialize();
+  VideoPlayerController? _controller;
+  final imagePicker = ImagePicker();
+  Future getVideoFromCamera() async {
+    // ignore: deprecated_member_use
+    final pickedFile = await imagePicker.getVideo(source: ImageSource.camera);
+    _controller = VideoPlayerController.file(File(pickedFile!.path));
+    _controller!.initialize().then((_) {
+      setState(() {
+        _controller!.play();
+      });
+    });
   }
-
-  @override
-  void dispose() {
-    // ウィジェットが破棄されたら、コントローラーを破棄
-    _controller.dispose();
-    super.dispose();
+  Future getVideoFromGarally() async {
+    PickedFile pickedFile =
+        // ignore: deprecated_member_use
+        (await imagePicker.getVideo(source: ImageSource.gallery))!;
+    _controller = VideoPlayerController.file(File(pickedFile.path));
+    _controller!.initialize().then((_) {
+      setState(() {
+        _controller!.play();
+      });
+    });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FutureBuilder<void>(
-          future: _initializeControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              //sleep(const Duration(seconds: 3));
-              return CameraPreview(_controller);
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
+        appBar: AppBar(
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // 写真を撮る
-          final image = await _controller.takePicture();
-          // 表示用の画面に遷移
-          // ignore: use_build_context_synchronously
-          await Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => DisplayPictureScreen(imagePath: image.path),
-              fullscreenDialog: true,
-            ),
-          );
-        },
-        child: const Icon(Icons.camera_alt),
-      ),
-    );
-  }
-}
-
-// 撮影した写真を表示する画面
-class DisplayPictureScreen extends StatelessWidget {
-  const DisplayPictureScreen({Key? key, required this.imagePath})
-      : super(key: key);
-
-  final String imagePath;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('撮れた写真')),
-      body: Center(child: Image.file(File(imagePath))),
-    );
+        body: Center(
+            // ignore: unnecessary_null_comparison
+            child: _controller == null
+                ? Text(
+                    '動画を選択してください',
+                    style: Theme.of(context).textTheme.headline4,
+                  )
+                : VideoPlayer(_controller!)),
+        floatingActionButton:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          FloatingActionButton(
+              onPressed: getVideoFromCamera, child: const Icon(Icons.video_call)),
+          FloatingActionButton(
+              onPressed: getVideoFromGarally, child: const Icon(Icons.movie_creation))
+        ]));
   }
 }
