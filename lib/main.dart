c4bc0778b02d628c42041653b0de864cf882437f
Koki Wakatsuki <hasukiiq@gmail.com>
2022-12-01 00:25:31 +0900

    startVideoRecording()を使用した動画撮影 自宅PC

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,17 @@
 import 'package:camera/camera.dart';
-// ignore: unused_import
 import 'package:flutter/material.dart';
-import 'package:sporty/view/top.dart';
-import 'package:sporty/view/menu.dart';
-import 'package:sporty/view/test.dart';
-import 'package:sporty/view/test2.dart';
-import 'package:sporty/view/slow.dart';
-import 'package:sporty/view/delay.dart';
 
-List<CameraDescription> cameras = [];
+import 'video_recorder_screen.dart';
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   final cameras = await availableCameras();
-  final firstCamera = cameras.first;
-  
-  runApp(App(camera: firstCamera));
-}
+  final firstCamera = cameras.firstWhere((camera) {
+    return camera.lensDirection == CameraLensDirection.front;
+  });
 
-class App extends StatelessWidget {
-  const App({
-    Key? key, 
-    required this.camera,
-  }) : super(key: key);
-
-  final CameraDescription camera;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/top',
-      routes: {
-        '/top': (context) => const Top(),
-        '/menu': (context) => const Menu(),
-        '/test': (context) => Test(camera: camera),
-        '/test2': (context) => Test2(camera: camera),
-        '/slow': (context) => const Slow(),
-        '/delay': (context) => Delay(camera: camera),
-      },
-    );
-  }
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    home: VideoRecorderScreen(camera: firstCamera),
+  ));
 }
---
+++ lib/video_recorder_screen.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: use_build_context_synchronously, avoid_print
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'video_player_screen.dart';
+
+class VideoRecorderScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const VideoRecorderScreen({
+    super.key,
+    required this.camera,
+  });
+
+  @override
+  State<VideoRecorderScreen> createState() => _VideoRecorderScreenState();
+}
+
+class _VideoRecorderScreenState extends State<VideoRecorderScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  bool _isRecording = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Video recorder screen')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+
+            if (!mounted) {
+              return;
+            }
+
+            if (_isRecording) {
+              final video = await _controller.stopVideoRecording();
+
+              await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => VideoPlayerScreen(
+                    videoPath: video.path,
+                  ),
+                ),
+              );
+            } else {
+              await _controller.prepareForVideoRecording();
+              await _controller.startVideoRecording();
+            }
+
+            setState(() {
+              _isRecording = !_isRecording;
+            });
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: Icon(_isRecording ? Icons.stop : Icons.circle),
+      ),
+    );
+  }
+}
