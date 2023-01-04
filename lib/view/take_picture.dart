463c55ee6bed0e6e675cbf993c4eb22edd41c630
Koki Wakatsuki <hasukiiq@gmail.com>
2023-01-04 15:08:38 +0900

    ストレージの動画見た後戻れるようにした 自宅PC

--- lib/view/take_picture.dart
+++ lib/view/take_picture.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import, avoid_unnecessary_containers, sized_box_for_whitespace, unused_field, unused_local_variable, no_leading_underscores_for_local_identifiers, prefer_const_constructors
+// ignore_for_file: unused_import, avoid_unnecessary_containers, sized_box_for_whitespace, unused_field, unused_local_variable, no_leading_underscores_for_local_identifiers, prefer_const_constructors, deprecated_member_use, duplicate_ignore
 
 import 'dart:io';
 import 'package:camera/camera.dart';
@@ -7,6 +7,8 @@ import 'package:image_picker/image_picker.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 import 'dart:typed_data';
 
 
@@ -25,8 +27,11 @@ class TakePicture extends StatefulWidget {
 
 class TakePictureState extends State<TakePicture> {
   late File _image;
+  late VideoPlayerController _controller;
+  late ChewieController _chewieController;
   final imagePicker = ImagePicker();
   late PickedFile pickedFile;
+  bool _isVideoPlay = false;
 
   @override
   void initState() {
@@ -40,9 +45,20 @@ class TakePictureState extends State<TakePicture> {
   }
 
   Future getVideoFromGarally() async {
-    PickedFile pickedFile =
-        // ignore: deprecated_member_use
-        (await imagePicker.getVideo(source: ImageSource.gallery))!;
+    PickedFile pickedFile = (await imagePicker.getVideo(source: ImageSource.gallery))!;
+    _controller = VideoPlayerController.file(File(pickedFile.path));
+    await _controller.initialize();
+    _chewieController = ChewieController(
+      videoPlayerController: _controller,
+      autoPlay: true,
+      looping: false,
+      fullScreenByDefault: false,
+      zoomAndPan: true,
+      playbackSpeeds: const [0.1, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.9, 1]
+    );
+    setState(() {
+      _isVideoPlay = true;
+    });
   }
 
   @override
@@ -52,7 +68,8 @@ class TakePictureState extends State<TakePicture> {
       backgroundColor: Colors.black,
         body: Center(
           // ignore: unnecessary_null_comparison
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          child: _isVideoPlay == false
+          ? Column(mainAxisAlignment: MainAxisAlignment.start, children: [
             InkWell(
               onTap: () {
                 getVideoFromCamera();
@@ -61,7 +78,7 @@ class TakePictureState extends State<TakePicture> {
                 padding: const EdgeInsets.all(20),
                 color: Colors.green,
                 width: _screenSize.width,
-                height: _screenSize.width,
+                height: _screenSize.height * 0.5,
                 child: Center(
                   child: const Icon(
                     size: 50,
@@ -79,7 +96,7 @@ class TakePictureState extends State<TakePicture> {
                 padding: const EdgeInsets.all(20),
                 color: Colors.blue,
                 width: _screenSize.width,
-                height: _screenSize.width,
+                height: _screenSize.height * 0.5,
                 child: Center(
                   child: const Icon(
                     size: 50,
@@ -90,6 +107,30 @@ class TakePictureState extends State<TakePicture> {
               ),
             ),
             ]
+          )
+          : Column(children: [
+            InkWell(
+              onTap: () {
+                setState(() {
+                  _isVideoPlay = false;
+                });
+              },
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                color: Colors.white,
+                width: _screenSize.width,
+                height: _screenSize.height * 0.1,
+                child: Center(
+                  child: Text('戻る'),
+                  ),
+                ),
+              ),
+            Container(
+              width: _screenSize.width,
+              height: _screenSize.height * 0.9,
+              child: Chewie(controller: _chewieController)
+            ),
+            ],
           )
         ),
     );
