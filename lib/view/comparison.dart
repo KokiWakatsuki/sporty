2aca4035e872f06c2616c19850cea3b46cd8f1c0
crescent_shape <hasukiiq@gmail.com>
2023-01-22 16:03:52 +0900

    縦並びと横並び対応 戻るボタンでデータ破棄するコード加えてない 自宅PC

--- lib/view/comparison.dart
+++ lib/view/comparison.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import, avoid_unnecessary_containers, sized_box_for_whitespace, unused_field, prefer_const_constructors, no_leading_underscores_for_local_identifiers, constant_identifier_names, sort_child_properties_last, deprecated_member_use, duplicate_ignore, avoid_print
+// ignore_for_file: unused_import, avoid_unnecessary_containers, sized_box_for_whitespace, unused_field, prefer_const_constructors, no_leading_underscores_for_local_identifiers, constant_identifier_names, sort_child_properties_last, deprecated_member_use, duplicate_ignore, avoid_print, prefer_final_fields
 
 import 'dart:io';
 import 'package:camera/camera.dart';
@@ -28,6 +28,8 @@ class _ComparisonState extends State<Comparison> {
   final imagePicker = ImagePicker();
   bool _isVideoSet_1 = false;
   bool _isVideoSet_2 = false;
+  bool _isTypeSetPage = false;
+  bool _isTypeSet = false;
   static const double main_text_size = 25;
   static const double sub_text_size = 17;
   static const double space_text_size = 10;
@@ -48,8 +50,9 @@ class _ComparisonState extends State<Comparison> {
   Future getVideoFromCamera_2() async {
     // ignore: deprecated_member_use
     pickedFile_2 = (await imagePicker.getVideo(source: ImageSource.camera))!;
-    _isVideoSet_2 = true;
-    setVideo();
+    setState(() {
+      _isVideoSet_2 = true;
+    });
   }
 
   Future getVideoFromGarally_1() async {
@@ -65,7 +68,20 @@ class _ComparisonState extends State<Comparison> {
     pickedFile_2 =
         // ignore: deprecated_member_use
         (await imagePicker.getVideo(source: ImageSource.gallery))!;
-    _isVideoSet_2 = true;
+        setState(() {
+          _isVideoSet_2 = true;
+        });
+  }
+
+  Future setType_1() async {
+    _isTypeSetPage = true;
+    _isTypeSet = false;
+    setVideo();
+  }
+
+  Future setType_2() async {
+    _isTypeSetPage = true;
+    _isTypeSet = true;
     setVideo();
   }
 
@@ -189,9 +205,7 @@ class _ComparisonState extends State<Comparison> {
                           ),
                           onPressed: () {
                             setState(() {
-                              setState(() {
-                                _isVideoSet_1 = false;
-                              });
+                              _isVideoSet_1 = false;
                             });
                           },
                           child:
@@ -252,33 +266,134 @@ class _ComparisonState extends State<Comparison> {
                       ),
                 ),
                 ]
-              )
-              : Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              ) :_isTypeSetPage == false
+              ? Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
                 Align(
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.black,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.black,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isVideoSet_2 = false;
+                            });
+                          },
+                          child:
+                              const Icon(color: Colors.white, Icons.arrow_back),
                       ),
-                      onPressed: () {
-                        setState(() {
-                          Navigator.of(context).pop();
-                        });
-                      },
-                      child:
-                          const Icon(color: Colors.white, Icons.arrow_back),
-                  ),
-                  alignment: Alignment.topLeft,
+                      alignment: Alignment.topLeft,
+                    ),
+                InkWell(
+                  onTap: () {
+                    setType_1();
+                  },
+                  child: Container(
+                        padding: const EdgeInsets.all(20),
+                        width: _screenSize.width * 0.85,
+                        height: _screenSize.height * 0.43,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.grey,
+                        ),
+                        child: Center(
+                          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text('STEP 3', style: TextStyle(fontSize: main_text_size,),),
+                              Text(' ', style: TextStyle(fontSize: space_text_size,),),
+                              Text('縦に並べる', style: TextStyle(fontSize: sub_text_size,),),
+                              Icon(
+                                size: 50, color: Colors.black, Icons.swap_vert
+                              ),
+                            ],
+                          ) 
+                        ),
+                      ),
                 ),
-                Container(
-                  height:MediaQuery.of(context).size.height * 0.45,
-                  child: Chewie(controller: _chewieController[0]),
-                ),
-                Container(
-                  height:MediaQuery.of(context).size.height * 0.45,
-                  child: Chewie(controller: _chewieController[1]),
+                InkWell(
+                  onTap: () {
+                    setType_2();
+                  },
+                  child: Container(
+                        padding: const EdgeInsets.all(20),
+                        width: _screenSize.width * 0.85,
+                        height: _screenSize.height * 0.43,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.grey,
+                        ),
+                        child: Center(
+                          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text('STEP 3', style: TextStyle(fontSize: main_text_size,),),
+                              Text(' ', style: TextStyle(fontSize: space_text_size,),),
+                              Text('横に並べる', style: TextStyle(fontSize: sub_text_size,),),
+                              Icon(
+                                size: 50, color: Colors.black, Icons.sync_alt
+                              ),
+                            ],
+                          )
+                        )
+                      ),
                 ),
                 ]
               )
+              : _isTypeSet == false
+                ? Stack(children: [
+                  Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                    Container(
+                      height:MediaQuery.of(context).size.height * 0.45,
+                      child: Chewie(controller: _chewieController[0]),
+                    ),
+                    Container(
+                      height:MediaQuery.of(context).size.height * 0.45,
+                      child: Chewie(controller: _chewieController[1]),
+                    ),
+                    ]
+                  ),
+                  Align(
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.black,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            Navigator.of(context).pop();
+                          });
+                        },
+                        child:
+                            const Icon(color: Colors.white, Icons.arrow_back),
+                    ),
+                    alignment: Alignment.topLeft,
+                  ),                    
+                ],)
+                : Stack(children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                    Container(
+                      width:MediaQuery.of(context).size.width * 0.45,
+                      child: Chewie(controller: _chewieController[0]),
+                    ),
+                    Container(
+                      width:MediaQuery.of(context).size.width * 0.45,
+                      child: Chewie(controller: _chewieController[1]),
+                    ),
+                    ]
+                  ),
+                  Align(
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.black,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            Navigator.of(context).pop();
+                          });
+                        },
+                        child:
+                            const Icon(color: Colors.white, Icons.arrow_back),
+                    ),
+                    alignment: Alignment.topLeft,
+                  ),
+              ],)
         ),
     );
   }
